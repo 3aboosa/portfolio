2e13d460c6fa5f840518d334a38d1cbbd904be61
3aboosa <boos@boosa.dev>
2025-03-22 03:46:06 -0400

    fixed type for Firefox browser detection

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ button.id = 'hover-button';
 button.innerText = 'Hover over me';
 document.querySelector('.img-div').appendChild(button);
 
-if (navigator.userAgent.includes("Chrome") 
-  || navigator.userAgent.includes("Mozilla")
+  if (navigator.userAgent.includes("Firefox") //fixed spelling
+  || navigator.userAgent.includes("Chrome") 
   || navigator.userAgent.includes("Edge"))
   {
-    applyCursor("#hover-button", "src/cursors/point_click.ani");
+    applyCursor("#hover-button", "src/cursors/aero_link.ani");
   }
