bd01594522fa773b73eff330b70f6334008d9fd5
3aboosa <boos@boosa.dev>
2025-03-25 01:47:47 -0400

    added mail icon animation

--- src/animator.js
+++ src/animator.js
@@ -2,7 +2,19 @@ import { DotLottie } from '@lottiefiles/dotlottie-web';
 
 const paperPlane = new DotLottie({
   autoplay: false,
-  loop: true,
+  loop: false,
   canvas: document.getElementById("paper-plane"),
   src: "src/animations/paper_plane.lottie"
 });
+
+const canvas = document.querySelector("canvas");
+
+canvas.addEventListener("mouseover", (event) => {
+  paperPlane.setLoop(true)
+  paperPlane.play();
+})
+
+canvas.addEventListener("mouseout", (event) => {
+  paperPlane.setLoop(false)
+});
+
