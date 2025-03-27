828a40585aaaf8d46d48ac425b025ab5d4a88919
3aboosa <boos@boosa.dev>
2025-03-27 03:20:25 -0400

    initial website redesign

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import { applyCursor } from './scripts/cursor.js'
 
-  if (navigator.userAgent.includes("Firefox")
-  || navigator.userAgent.includes("Chrome") 
-  || navigator.userAgent.includes("Edge"))
-  {
-    applyCursor(".nav-bar-logo-link", "src/cursors/point_house.ani");
-    applyCursor(".nav-bar-options-link", "src/cursors/point_click_right.ani");
+  // if (navigator.userAgent.includes("Firefox")
+  // || navigator.userAgent.includes("Chrome") 
+  // || navigator.userAgent.includes("Edge"))
+  // { 
 
-    applyCursor("#maintenance-img", "src/cursors/grab.ani");
-
-    applyCursor(".footer-links-img", "src/cursors/point_click_right.ani");
-    applyCursor(".footer-links-email-canvas", "src/cursors/point_click_right.ani");
-  }
+  // }
